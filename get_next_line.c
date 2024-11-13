47f3a0bb0de67b9631197a1da1cf1eacffc32f23
Nahil13ieme <nahil@Nahil.>
2024-11-13 19:51:28 +0100

    almost working. Some errors in memory.

--- get_next_line.c
+++ get_next_line.c
@@ -13,48 +13,49 @@
 #include "get_next_line.h"
 #include <stdlib.h>
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	static char *staticBuffer;
-	char *buffer;
-	char *line;
-	char *res;
-	size_t readBytes;
+	static char	*staticBuffer;
+	char		*buffer;
+	char		*line;
+	char		*res;
+	size_t		readBytes;
 
 	buffer = malloc(BUFFER_SIZE + 1);
 	readBytes = read(fd, buffer, BUFFER_SIZE);
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	while(readBytes > 0)
+	while (readBytes > 0)
 	{
 		buffer[BUFFER_SIZE] = 0;
 		staticBuffer = ft_strjoin_free(staticBuffer, buffer);
 		line = ft_strchr(staticBuffer, '\n');
 		if(line)
 		{
-			printf("staticBuffer = %s", staticBuffer);
 			line = ft_strchr(staticBuffer, '\n');
-			*line++ = 0;
 			res = ft_strdup_line(staticBuffer);
-			staticBuffer = line;
+			*line++ = 0;
+			staticBuffer = ft_strdup(line);
 			free(buffer);
-			return (ft_strdup(res));
+			printf("line = '%s'\n", line);
+			return (res);
 		}
 		readBytes = read(fd, buffer, BUFFER_SIZE);
-		// If i can read i need to add the buffer in the staticBuffer but if i cannot read i need to make sure the 
-		// static buffer is empty too
 	}
-	if(staticBuffer)
+	if (staticBuffer)
 	{
-		printf("staticBuffer = %s", staticBuffer);
+		printf("staticBuffer = '%s'\n", staticBuffer);
 		line = ft_strchr(staticBuffer, '\n');
-		*line++ = 0;
+		if (line == NULL)
+			line = ft_strchr(staticBuffer, 0);
 		res = ft_strdup_line(staticBuffer);
-		staticBuffer = line;
+		*line++ = 0;
+		staticBuffer = ft_strdup(line);
 		free(buffer);
-		return (ft_strdup(res));
+		if(staticBuffer == NULL)
+			free(staticBuffer);
+		return (res);
 	}
 	free(buffer);
 	return (NULL);
-	// Check if staticBuffer is empty, if not i need to return line by line
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -16,6 +16,8 @@ int	ft_strlen(char *s)
 {
 	int	i;
 
+	if (!s)
+		return (0);
 	i = 0;
 	while(s[i])
 		i++;
@@ -24,26 +26,30 @@ int	ft_strlen(char *s)
 
 char	*ft_strjoin_free(char *s1, char *s2)
 {
-	char	*res;
-	char	*tmp;
+	char			*array;
+	unsigned int	size;
+	int				i;
+	int				j;
 
-	if(!s1 && !s2)
+	if (!s1 && !s2)
 		return (NULL);
-	if (!s1)
-		return (s2);
-	if (!s2)
-		return (s1);
-	res = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	tmp = res;
-	if (!res)
+	size = ft_strlen((s1)) + ft_strlen(s2);
+	array = (char *)malloc(sizeof(char) * (size + 1));
+	if (array == NULL)
 		return (NULL);
-	while(*s1)
-		*res++ = *s1++;
-	while(*s2)
-		*res++ = *s2++;
-	*res = 0;
-	free(s1 - sizeof(s1));
-	return (tmp);
+	i = 0;
+	j = 0;
+	if (s1)
+	{
+		while (s1[i])
+			array[j++] = s1[i++];
+		i = 0;
+	}
+	while (s2[i])
+		array[j++] = s2[i++];
+	array[size] = '\0';
+	free(s1);
+	return (array);
 }
 
 char	*ft_strdup(char *s)
@@ -61,21 +67,38 @@ char	*ft_strdup(char *s)
 	return (copy - len);
 }
 
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (size == 0)
+		return (ft_strlen(src));
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(src));
+}
+
 char	*ft_strdup_line(char *s)
 {
 	size_t	len;
 	char	*copy;
 
 	len = 0;
-	while(s[len] && s[len] != '\n')
+	printf("s = '%s'\n", s);
+	while (s[len] && s[len] != '\n')
+		len++;
+	if (len == 0)
+		return (NULL);
+	if (s[len] == '\0')
 		len++;
 	copy = (char *)malloc(len+1);
-	if (copy == NULL)
-		return (NULL);
-	while (*s)
-		*copy++ = *s++;
-	*copy = '\0';
-	return (copy - len);
+	ft_strlcpy(copy, s, len);
+	return (copy);
 }
 
 char	*ft_strchr(char *s,	int c)
@@ -83,7 +106,7 @@ char	*ft_strchr(char *s,	int c)
 	char	*ptr;
 
 	ptr = s;
-	while(*ptr)
+	while (*ptr)
 	{
 		if (*ptr == (char)c)
 			return (ptr);
